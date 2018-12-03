82a59527ef069619ea0f6d7544e0c3325294c001
Aleix <nigeon@gmail.com>
2018-12-03 18:01:49 +0100

    Added some templating for the tests

--- test/test.js
+++ test/test.js
@@ -1,9 +1,52 @@
-//var assert = require('assert');
-var assert = require('chai').assert;
+var assert = require('assert');
+var expect = require('chai').expect;
+var should = require('chai').should();
+
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
       assert.equal([1,2,3].indexOf(4), -1);
     });
   });
+});
+
+describe('Get Process Metadata', function(){
+  it('Should return the mock process Metadata');
+});
+
+describe('EncryptVote', function(){
+  it('');
+});
+describe('HashEncryptedVote', function(){
+  it('');
+});
+describe('GenerateProof', function(){
+  it('');
+});
+describe('getNullifyer', function(){
+  it('');
+});
+describe('generatePOW', function(){
+  it('');
+});
+describe('getVotingPackage', function(){
+  it('');
+});
+describe('getRelays', function(){
+  it('');
+});
+describe('encryptRelayPackage', function(){
+  it('');
+});
+describe('sendPackage', function(){
+  it('');
+});
+describe('getVotesBatchHashById', function(){
+  it('');
+});
+describe('fetchExternalData', function(){
+  it('');
+});
+describe('batchExists', function(){
+  it('');
 });
