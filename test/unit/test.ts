2e20a60abce9b056f0296585db38a3fd7246af5d
Aleix <nigeon@gmail.com>
2018-12-10 16:08:51 +0100

    Added web3 with TS support. Test available to see it running.

--- test/unit/test.ts
+++ test/unit/test.ts
@@ -1,4 +1,5 @@
 import {assert} from "chai";
+import Web3 = require("web3");
 
 describe("Array", () =>  {
   describe("#indexOf()", () =>  {
@@ -10,4 +11,12 @@ describe("Array", () =>  {
 
 describe("fetchExternalData", () => {
   it("");
+});
+
+describe("web3 dependency", () => {
+  it("works", async () => {
+    const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    const coinbase = await web3.eth.getCoinbase();
+    assert.equal(coinbase, "0x6172c8fc696133385f89fa4ca687c59ad288b37a");
+  });
 });
