fffe31f71dc11092a9ae370002088f09587b9e52
Aleix <nigeon@gmail.com>
2018-12-11 15:52:06 +0100

    Environment configuration. getProcessMetadata working with web3 in local. BUG trying to load the Contract/ABI file, preventing the test the work

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -1,5 +1,27 @@
+import * as fs from "fs";
+import Web3 = require("web3");
+
 export default class Blockchain {
-    public static getProcessMetadata(id: number): object {
-        return {};
+    private url: string;
+    private web3: Web3;
+    private votingProcessContractAbiPath: string;
+    private votingProcessContractAbi: any[];
+    private votingProcessContractAddress: string;
+
+    constructor(url: string, votingProcessContractAbiPath, votingProcessContractAddress) {
+        this.url = url;
+        this.web3 = new Web3(new Web3.providers.HttpProvider(url));
+        this.votingProcessContractAddress = votingProcessContractAddress;
+
+        this.votingProcessContractAbiPath = votingProcessContractAbiPath;
+        const parsed = JSON.parse(fs.readFileSync(__dirname + "/.." + votingProcessContractAbiPath).toString());
+        this.votingProcessContractAbi = parsed.abi;
+    }
+
+    public async getProcessMetadata(id: string): Promise<any> {
+        const votingProcessContract = new this.web3.eth.Contract(this.votingProcessContractAbi,
+                                                                this.votingProcessContractAddress);
+        id = this.web3.utils.asciiToHex(id);
+        return await votingProcessContract.methods.getProcessMetadata(this.web3.utils.hexToBytes(id)).call();
     }
 }
