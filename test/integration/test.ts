5a6047650968bdcbe7624f82cbc2e62d63137894
Aleix <nigeon@gmail.com>
2018-12-13 10:35:07 +0100

    Removed dotenv and config files. This is a library, there's no environment config here.

--- test/integration/test.ts
+++ test/integration/test.ts
@@ -1,10 +1,10 @@
 import {assert} from "chai";
 import Web3 = require("web3");
-import Config from "../../src/utils/config";
-
 describe("Web3 dependency", () => {
+    const blockchainUrl: string = "http://localhost:8545";
+
     it("Connects to localhost rpc and gets coinbase address", async () => {
-        const web3 = new Web3(new Web3.providers.HttpProvider(Config.BLOCKCHAIN_URL));
+        const web3 = new Web3(new Web3.providers.HttpProvider(blockchainUrl));
         const coinbase = await web3.eth.getCoinbase();
         assert.isNotEmpty(coinbase);
     });
