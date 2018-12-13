1b8124bb1e5d470d0e328a6edf28384a1e7fc1af
Aleix <nigeon@gmail.com>
2018-12-13 10:24:44 +0100

    Normalized indentation to 4 spaces...

--- test/unit/process.ts
+++ test/unit/process.ts
@@ -6,88 +6,87 @@ import Process from "../../src/process";
 import Config from "../../src/utils/config";
 
 describe("Process", () => {
-  const blockchainUrl = Config.BLOCKCHAIN_URL;
-  const votingProcessContractPath = Config.VOTING_PROCESS_CONTRACT_PATH;
-  const votingProcessContractAddress = Config.VOTING_PROCESS_CONTRACT_ADDRESS;
-  let process;
+    const blockchainUrl: string = Config.BLOCKCHAIN_URL;
+    const votingProcessContractPath: string = Config.VOTING_PROCESS_CONTRACT_PATH;
+    const votingProcessContractAddress: string = Config.VOTING_PROCESS_CONTRACT_ADDRESS;
+    let process: Process;
 
-  beforeEach(() => {
-    const getVotingProcessAbi = sinon.stub(Blockchain.prototype, "getVotingProcessContractAbi")
-                                     .returns([{}]);
-    process = new Process(blockchainUrl, votingProcessContractPath, votingProcessContractAddress);
-    getVotingProcessAbi.restore();
-  });
-
-  describe("#GetById", () => {
-    it("Should not accept an invalid id", () => {
-      assert.throws(() => { process.getMetadata(""); }, Error, "ID can't be empty");
+    beforeEach(() => {
+        const getVotingProcessAbi = sinon.stub(Blockchain.prototype, "getVotingProcessContractAbi")
+                                         .returns([{}]);
+        process = new Process(blockchainUrl, votingProcessContractPath, votingProcessContractAddress);
+        getVotingProcessAbi.restore();
     });
 
-    it("Should return a valid process Metadata", async () => {
-      const expectedProcessMetadata: object = {
-        censusRoot: "",
-        censusUrl: "",
-        endBlock: "",
-        startBlock: "",
-        status: "",
-        trustedGateways: "",
-        voteEncryptionKeys: "",
-        votingOptions: "",
-      };
+    describe("#GetById", () => {
+        it("Should not accept an invalid id", () => {
+            assert.throws(() => { process.getMetadata(""); }, Error, "ID can't be empty");
+        });
 
-      const getProcessMetadataStub = sinon.stub(Blockchain.prototype, "getProcessMetadata")
+        it("Should return a valid process Metadata", async () => {
+            const expectedProcessMetadata: object = {
+                censusRoot: "",
+                censusUrl: "",
+                endBlock: "",
+                startBlock: "",
+                status: "",
+                trustedGateways: "",
+                voteEncryptionKeys: "",
+                votingOptions: "",
+            };
+
+            const getProcessMetadataStub = sinon.stub(Blockchain.prototype, "getProcessMetadata")
                                                 .resolves(expectedProcessMetadata);
 
-      const metadata: object = await process.getMetadata("identifier");
+            const metadata: object = await process.getMetadata("identifier");
 
-      getProcessMetadataStub.restore();
-      sinon.assert.match(metadata, expectedProcessMetadata);
-    });
-  }),
-
-  describe("#getOpen", () => {
-    it("");
-  }),
-
-  describe("#getRelays", () => {
-    it("");
-  }),
-
-  describe("#batchExists", () => {
-    it("");
-  }),
-
-  describe("#getVotingOptions", () => {
-    it("");
-  }),
-
-  describe("#encryptVote", () => {
-    it("Fails on empty vote", () => {
-      const vote: string = "";
-      const votePublicKey: string = "123abcdeb";
-      assert.throws(() => { process.encryptVote(vote, votePublicKey); }, Error, "Vote can't be empty");
+            getProcessMetadataStub.restore();
+            sinon.assert.match(metadata, expectedProcessMetadata);
+        });
     }),
 
-    it("Fails on empty votePublicKey", () => {
-      const vote: string = "1";
-      const votePublicKey: string = "";
-      assert.throws(() => { process.encryptVote(vote, votePublicKey); }, Error, "VotePublicKey can't be empty");
+    describe("#getOpen", () => {
+        it("");
     }),
 
-    it("Result is a String", () => {
-      const vote: string = "1";
-      const votePublicKey: string = "123abcdeb";
-      const encryptedVote: string = process.encryptVote(vote, votePublicKey);
-      assert.isString(encryptedVote);
+    describe("#getRelays", () => {
+        it("");
+    }),
+
+    describe("#batchExists", () => {
+        it("");
+    }),
+
+    describe("#getVotingOptions", () => {
+        it("");
+    }),
+
+    describe("#encryptVote", () => {
+        it("Fails on empty vote", () => {
+            const vote: string = "";
+            const votePublicKey: string = "123abcdeb";
+            assert.throws(() => { process.encryptVote(vote, votePublicKey); }, Error, "Vote can't be empty");
+        }),
+
+        it("Fails on empty votePublicKey", () => {
+            const vote: string = "1";
+            const votePublicKey: string = "";
+            assert.throws(() => { process.encryptVote(vote, votePublicKey); }, Error, "VotePublicKey can't be empty");
+        }),
+
+        it("Result is a String", () => {
+            const vote: string = "1";
+            const votePublicKey: string = "123abcdeb";
+            const encryptedVote: string = process.encryptVote(vote, votePublicKey);
+            assert.isString(encryptedVote);
+        });
+    }),
+
+    describe("#hashEncryptedVote", () => {
+        it("");
+    }),
+
+    describe("#getVotingPackage", () => {
+        it("");
     });
-  }),
-
-  describe("#hashEncryptedVote", () => {
-    it("");
-  }),
-
-  describe("#getVotingPackage", () => {
-    it("");
-  });
-
 });
--- test/unit/test.ts
+++ test/unit/test.ts
@@ -2,13 +2,13 @@ import {assert} from "chai";
 import Web3 = require("web3");
 
 describe("Array", () =>  {
-  describe("#indexOf()", () =>  {
-    it("should return -1 when the value is not present", () =>  {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+    describe("#indexOf()", () =>  {
+        it("should return -1 when the value is not present", () =>  {
+            assert.equal([1, 2, 3].indexOf(4), -1);
+        });
     });
-  });
 });
 
 describe("fetchExternalData", () => {
-  it("");
+    it("");
 });
