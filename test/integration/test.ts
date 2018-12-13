a1a4e2632179e1fd3fa5b44f580c17373040d819
Aleix <nigeon@gmail.com>
2018-12-13 10:24:44 +0100

    Normalized indentation to 4 spaces...

--- test/integration/test.ts
+++ test/integration/test.ts
@@ -3,9 +3,9 @@ import Web3 = require("web3");
 import Config from "../../src/utils/config";
 
 describe("Web3 dependency", () => {
-  it("Connects to localhost rpc and gets coinbase address", async () => {
-    const web3 = new Web3(new Web3.providers.HttpProvider(Config.BLOCKCHAIN_URL));
-    const coinbase = await web3.eth.getCoinbase();
-    assert.isNotEmpty(coinbase);
-  });
+    it("Connects to localhost rpc and gets coinbase address", async () => {
+        const web3 = new Web3(new Web3.providers.HttpProvider(Config.BLOCKCHAIN_URL));
+        const coinbase = await web3.eth.getCoinbase();
+        assert.isNotEmpty(coinbase);
+    });
 });
