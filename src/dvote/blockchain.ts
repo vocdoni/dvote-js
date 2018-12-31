61a11b5d92e9358f46f52b34ce4d94dadeff6b1c
Xavi <xavings@gmail.com>
2018-12-31 08:27:44 +0000

    VotingProcess abi is provided through the dvote-smart-contracts npm module

--- src/dvote/blockchain.ts
+++ src/dvote/blockchain.ts
@@ -1,22 +1,21 @@
+import DvoteContracts = require("dvote-smart-contracts");
 import * as fs from "fs";
 import Web3 = require("web3");
 import Contract from "web3/eth/contract";
 
 export default class Blockchain {
     private url: string;
-    private contractPath: string;
+    // private contractPath: string;
     private contractAbi: any[];
     private contractAddress: string;
     private contract: Contract;
     private web3: Web3;
 
-    constructor(url: string, contractPath, contractAddress) {
+    constructor(url: string, contractAddress) {
         this.url = url;
         this.web3 = new Web3(new Web3.providers.HttpProvider(url));
         this.contractAddress = contractAddress;
-
-        this.contractPath = contractPath;
-        this.contractAbi = this.getContractAbi(contractPath);
+        this.contractAbi = DvoteContracts.VotingProcess.abi;
 
         this.contract = new this.web3.eth.Contract(this.contractAbi, this.contractAddress);
     }
