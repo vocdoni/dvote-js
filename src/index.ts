83674dfe7c05eda746f8135c0882ce0a72027259
Xavi <xavings@gmail.com>
2018-12-31 19:53:22 +0000

    Adding Census and test WIP

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as Process } from "./dvote/process";
 export { default as Blockchain } from "./dvote/blockchain";
 export { default as Entity } from "./dvote/entity";
+export { default as Census } from "./dvote/census";
