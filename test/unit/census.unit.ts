5187ce08c6311cea3f503a2898f0de4e4482aa65
Xavi <xavings@gmail.com>
2018-12-31 19:53:22 +0000

    Adding Census and test WIP

--- test/unit/census.unit.ts
+++ test/unit/census.unit.ts
@@ -1,9 +1,39 @@
+import * as nodeassert from "assert";
+import { assert } from "chai";
+import * as sinon from "sinon";
+
+import * as dvote from "../../src";
+import MerkleProof from "../../src/dvote/merkleProof";
+
 describe("Census", () => {
+    const blockchainUrl: string = "http://localhost:8545";
+    const votingProcessContractAddress: string = "0xd8c3d0B72DFbE3adbe0fd9295c9fe083ff896684";
+    let census: dvote.Census;
+
+    beforeEach(() => {
+        census = new dvote.Census(blockchainUrl, votingProcessContractAddress);
+    });
+
     describe("#getCensus()", () => {
         it("");
     }),
 
-    describe("#getCensusProof()", () => {
+    describe("#getFranchiseProofUrl()", () => {
         it("");
     });
+
+    describe("#getCensusProof()", () => {
+
+        it("Result is a String", async () => {
+                const franchiseProofUrl: string = "http://vocdoni.io/getFranchiseProof";
+                const votePublicKey: string = "123abcdeb";
+                const censusProof: MerkleProof = await census.getProof(votePublicKey, franchiseProofUrl);
+                assert.isArray(censusProof);
+        });
+    });
+
+    describe("#VerifiyCensusProof()", () => {
+        it("");
+    });
+
 });
