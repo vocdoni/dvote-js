055ea8a7f914cf60fc36bd162bcfa2aded6f4d84
Xavi <xavings@gmail.com>
2019-01-02 10:30:48 +0000

    Checking for MerkleProof instead of array. Typo

--- src/dvote/merkleProof.ts
+++ src/dvote/merkleProof.ts
@@ -2,12 +2,12 @@ export default class MerkleProof {
 
     public root: string;
     public siblings: string[];
-    public leave: string;
+    public leaf: string;
 
     constructor(proof: string[]) {
         this.root = proof[0];
         this.siblings = proof.slice(1, proof.length - 2);
-        this.leave = proof[proof.length - 1];
+        this.leaf = proof[proof.length - 1];
     }
 
 }
