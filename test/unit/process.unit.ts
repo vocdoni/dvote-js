8fdd5399b83848c48645ae85caa83e16aa1299bb
Xavi <xavings@gmail.com>
2019-01-05 13:58:55 +0000

    VotingContract address in process.unit comes from just deployed contract

--- test/unit/process.unit.ts
+++ test/unit/process.unit.ts
@@ -1,18 +1,44 @@
 import * as nodeassert from "assert";
 import { assert } from "chai";
+import DvoteSmartContracts = require("dvote-smart-contracts");
 import * as sinon from "sinon";
 import * as dvote from "../../src";
 
-describe("Process", () => {
+import Web3 = require("web3");
+import Web3Personal = require("web3-eth-personal");
+describe("#Unit Process", () => {
     const blockchainUrl: string = "http://localhost:8545";
-    const votingProcessContractAddress: string = "0xd8c3d0B72DFbE3adbe0fd9295c9fe083ff896684";
+    const web3: Web3 = new Web3(new Web3.providers.HttpProvider(blockchainUrl));
+    const web3Personal = new Web3Personal(blockchainUrl);
+    let votingProcessContractAddress: string = null;
     let process: dvote.Process;
 
-    beforeEach(() => {
-        process = new dvote.Process(blockchainUrl, votingProcessContractAddress);
-    });
-
     describe("#GetById", () => {
+
+        it("Should deploy a new VotingProcess contract", async () => {
+
+            const accounts = await web3Personal.getAccounts();
+            const organizer1 = accounts[0];
+            const deployGasCost = 2600000;
+            const deployGasPrice = web3.utils.toWei("1.2", "Gwei");
+
+            const contract = new web3.eth.Contract(DvoteSmartContracts.VotingProcess.abi);
+            const deployTransaction = await contract.deploy({
+                data: DvoteSmartContracts.VotingProcess.bytecode,
+            });
+
+            const instance = await deployTransaction.send({
+                from: organizer1,
+                gas: deployGasCost,
+                gasPrice: deployGasPrice,
+            });
+
+            votingProcessContractAddress = instance.options.address;
+            process = new dvote.Process(blockchainUrl, votingProcessContractAddress);
+
+            assert.isString(votingProcessContractAddress);
+        });
+
         it("Should not accept an invalid id", () => {
             nodeassert.rejects(process.getMetadata(""), "Empty ID should fail");
         });
