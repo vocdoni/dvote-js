64adce59bf6f88f4d7a7c0d6b3a87dd2d5f3b516
Aleix <nigeon@gmail.com>
2019-01-09 12:44:44 +0100

    Unit testing shouldn't deploy contracts, as we are testing standalone functions, no external interactions.

--- src/dvote/blockchain.ts
+++ src/dvote/blockchain.ts
@@ -1,4 +1,3 @@
-import * as fs from "fs";
 import Web3 = require("web3");
 
 export default class Blockchain {
@@ -8,13 +7,17 @@ export default class Blockchain {
     private contract: Web3.eth.Contract;
     private web3: Web3;
 
-    constructor(url: string, contractAddress, contractAbi) {
+    constructor(url: string, contractAddress: string, contractAbi: any[]) {
         this.url = url;
         this.web3 = new Web3(new Web3.providers.HttpProvider(url));
         this.contractAddress = contractAddress;
         this.contractAbi = contractAbi;
 
-        this.contract = new this.web3.eth.Contract(this.contractAbi, this.contractAddress);
+        this.contract = this.getContract(this.contractAbi, this.contractAddress);
+    }
+
+    public getContract(contractAbi: any[], contractAddress: string) {
+        return new this.web3.eth.Contract(contractAbi, contractAddress);
     }
 
     public async exec(method: string, params?: any[], options?: any) {
