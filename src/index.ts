7eb753506933244fc3e8e521b18ba3d3bcf06cbc
Xavi <xavings@gmail.com>
2019-02-13 11:21:46 +0000

    Testing string/byte32 utils

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-export { default as Process } from "./dvote/process";
-export { default as Blockchain } from "./dvote/blockchain";
-export { default as Entity } from "./dvote/entity";
-export { default as Census } from "./dvote/census";
+export { default as Process } from "./dvote/process"
+export { default as Blockchain } from "./dvote/blockchain"
+export { default as Entity } from "./dvote/entity"
+export { default as Census } from "./dvote/census"
+export { default as Utils } from "./dvote/utils"
