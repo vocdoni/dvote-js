3c91ce95cdbd2dbc780ac2752c792533927ca03f
Xavi <xavings@gmail.com>
2019-04-08 15:23:06 +0000

    Fixing references

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { default as Process } from "./dvote/process"
 export { default as Blockchain } from "./dvote/blockchain"
-export { default as Entity } from "./dvote/entity"
+export { default as EntityResolver } from "./dvote/entityResolver"
 export { default as Census } from "./dvote/census"
 export { default as Utils } from "./dvote/utils"
