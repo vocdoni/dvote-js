797abc5ed2a73b53c1f0b265f2a52d44da62436a
Jør∂¡ <itsbrickpop@gmail.com>
2019-04-17 19:09:45 +0200

    Census service starter

--- src/dvote/census.ts
+++ src/dvote/census.ts
@@ -1,1 +1,34 @@
-export default {}
+// import { providers, utils } from "ethers"
+import Gateway from "./gateway"
+
+export default class Census {
+	gateway: Gateway = null
+	censusServiceAddress: string = null
+
+	// METHODS
+
+	/**
+	 * Creates a contract factory to deploy or attach to VotingProcess instances
+	 * @param params 
+	 */
+	constructor(gatewayWsUri: string, censusServiceAddress: string) {
+		this.gateway = new Gateway(gatewayWsUri)
+		this.censusServiceAddress = censusServiceAddress
+
+		throw new Error("unimplemented")
+	}
+
+	public async addClaim(processId: string, gatewayUri: string, nullifier: string): Promise<boolean> {
+		if (!this.censusServiceAddress) throw new Error("The census service address is empty")
+
+		// Ensure we are connected to the right Gateway
+		if (!this.gateway) this.gateway = new Gateway(gatewayUri)
+		else if (this.gateway.getUri() != gatewayUri) await this.gateway.setGatewayUri(gatewayUri)
+
+		throw new Error("unimplemented")
+
+		return this.gateway.request({
+			// method: "addClaim"
+		}).then(strData => JSON.parse(strData))
+	}
+}
