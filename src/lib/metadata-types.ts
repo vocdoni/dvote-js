c77645f6dc24ee185ebf5eff0e8bc02fdeae45a9
ledfuse <ledfuse@users.noreply.github.com>
2019-04-18 18:56:07 +0200

    Entity Metadata tpings

---
+++ src/lib/metadata-types.ts
@@ -0,0 +1,125 @@
+type ProtocolVersion = "1.0"
+type HexString = string
+type ContractAddress = HexString
+type PublicKey = HexString
+type PrivateKey = HexString
+type ProcessId = HexString           // Hash of the organizer's address and the nonce of the process
+
+type LocalizedText = {
+    [lang: string]: string           // Indexed by language  { en: "Hello", fr: "Salut", ... }
+}
+
+type ContentURI = string             // Comma-separated list of URI's (http://vocdoni.io/docs/#/architecture/protocol/data-origins?id=content-uri)
+type MessagingURI = string           // Comma-separated list of URI's (http://vocdoni.io/docs/#/architecture/protocol/data-origins?id=messaging-uri)
+type URL = string
+
+///////////////////////////////////////////////////////////////////////////////
+// ENTITY METADATA
+///////////////////////////////////////////////////////////////////////////////
+
+// More info: http://vocdoni.io/docs/#/architecture/components/entity?id=meta
+
+export interface EntityMetadata {
+    version: ProtocolVersion,             // Protocol version
+    languages: string[],                  // Two character language code (en, fr, it, ...)
+    "entity-name": string,
+    "entity-description": LocalizedText,
+    "voting-contract": ContractAddress,   // e.g. 0x1234567890123456789012345678901234567890
+    "gateway-update": {
+        timeout: number,                  // milliseconds after which a Gateway is marked as down
+        topic: string,                    // Topic used for the messaging protocol (e.g. "vocdoni-gateway-update")
+        difficulty: number                // Difficulty of the proof of work, to prevent spammers
+    },
+    "process-ids": {
+        active: ProcessId[],
+        ended: ProcessId[]
+    },
+    "news-feed": {
+        en: ContentURI,
+        fr: ContentURI
+    },
+    avatar: ContentURI,
+
+    // Bootnodes providing a list of active Gateways
+    "gateway-boot-nodes": GatewayBootNode[],
+
+    // List of currently active Relays. This list is just for informational purposes.
+    // The effective list of relays for a voting process is on the Voting Process smart contract
+    relays: RelayData[],
+
+    // List of custom interactions that the entity defines.
+    // It may include anything like visiting web sites, uploading pictures, making payments, etc.
+    actions: EntityCustomAction[]
+}
+
+interface GatewayBootNode {
+    update: MessagingURI,
+    fetch: URL
+}
+
+interface RelayData {
+    publicKey: PublicKey,  // Key to encrypt data sent to it
+    messagingUri: MessagingURI    // Where to send messages. See Data origins > Messaging URI
+}
+
+type EntityCustomAction = EntityBaseAction & (EntityBrowserAction | EntityImageUploadAction)
+
+// The common fields of any action
+interface EntityBaseAction {
+    // Localized Call To Action to appear on the app
+    name: LocalizedText,
+}
+
+// Opening an interactive web browser
+interface EntityBrowserAction {
+    type: "browser",
+
+    // The URL to open
+    // - The embedded web site can send messages to the host app
+    // - Messages can request the public key, or a signature
+    url: URL,
+
+    // Endpoint to POST to with the publicKey and a signed timestamp
+    // Returning true will show the action and hide it otherwise
+    // Entering "true" instead of a URL will show it always
+    visible: URL | true
+}
+
+// App-driven image upload example
+interface EntityImageUploadAction {
+    type: "image",
+
+    // Requested image types to provide
+    source: ImageUploadSource[],
+
+    // Endpoint accepting POST requests with JSON payloads like:
+    // {
+    //   "field-name-1": "base64-image-payload",
+    //   "another-field": "base64-image-payload",
+    //   ...
+    // }
+    //
+    // The URL will receive the following query string parameters:
+    // - signature = sign(hash(jsonBody), privateKey)
+    // - publicKey
+    url: URL,
+
+    // Endpoint to POST to with publicKey and signature+timestamp fields
+    // Returning true will show the action and hide it otherwise
+    visible: URL | true
+}
+
+type ImageUploadSource = {
+    type: "front-camera" | "back-camera" | "gallery",
+    name: string,                            // Arbitrary name to identify the data when the JSON is posted
+    orientation?: "portrait" | "landscape",  // Optional when type == "gallery"
+    overlay?: "face" | "id-card-front",
+    caption?: LocalizedText
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// VOTING PROCESS METADATA
+///////////////////////////////////////////////////////////////////////////////
+
+// More info: http://vocdoni.io/docs/#/architecture/components/process?id=process-metadata-json
