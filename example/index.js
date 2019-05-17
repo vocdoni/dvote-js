0a3518397c03c951d9b7eb0130cebe4b49566344
Jør∂¡ <itsbrickpop@gmail.com>
2019-05-17 11:21:54 +0200

    Adding some example code

---
+++ example/index.js
@@ -0,0 +1,68 @@
+const { EntityResolver } = require("..") // require("dvote-js")
+
+const MNEMONIC = ""
+const PROVIDER_URL = "http://..."
+const resolverContractAddress = "0x..."
+const myEntityAddress = "0x..."
+
+async function registerEntity() {
+    console.log("Attaching to", resolverContractAddress)
+
+    const EntityResolverFactory = new EntityResolver({ providerUrl: PROVIDER_URL, mnemonic: MNEMONIC })
+    const resolverInstance = EntityResolverFactory.attach(resolverContractAddress)
+
+    const entityId = EntityResolver.getEntityId(myEntityAddress)
+    console.log("Entity ID", entityId)
+
+    console.log("Setting values. Please wait...")
+
+    // setting values
+    let tx = await resolverInstance.setText(entityId, "vnd.vocdoni.entity-name", "My official entity")
+    await tx.wait()
+    tx = await resolverInstance.setText(entityId, "vnd.vocdoni.languages", JSON.stringify(["en", "fr"]))
+    await tx.wait()
+    tx = await resolverInstance.setText(entityId, "vnd.vocdoni.meta", 'bzz://12345,ipfs://12345')
+    await tx.wait()
+    tx = await resolverInstance.setText(entityId, "vnd.vocdoni.voting-contract", '0x0')
+    await tx.wait()
+    tx = await resolverInstance.setText(entityId, "vnd.vocdoni.gateway-update", JSON.stringify({ "timeout": 60000, "topic": "vocdoni-gateway-update", "difficulty": 1000 }))
+    await tx.wait()
+    tx = await resolverInstance.setText(entityId, "vnd.vocdoni.process-ids.active", JSON.stringify([]))
+    await tx.wait()
+    tx = await resolverInstance.setText(entityId, "vnd.vocdoni.process-ids.ended", JSON.stringify([]))
+    await tx.wait()
+    tx = await resolverInstance.setText(entityId, "vnd.vocdoni.news-feed.en", "https://hipsterpixel.co/feed.json")
+    await tx.wait()
+    tx = await resolverInstance.setText(entityId, "vnd.vocdoni.news-feed.fr", "https://feed2json.org/convert?url=http://www.intertwingly.net/blog/index.atom")
+    await tx.wait()
+    tx = await resolverInstance.setText(entityId, "vnd.vocdoni.entity-description.en", "The description of my entity goes here")
+    await tx.wait()
+    tx = await resolverInstance.setText(entityId, "vnd.vocdoni.entity-description.fr", "La description officielle de mon organisation est ici")
+    await tx.wait()
+    tx = await resolverInstance.setText(entityId, "vnd.vocdoni.avatar", "https://hipsterpixel.co/assets/favicons/apple-touch-icon.png")
+    await tx.wait()
+
+    // show stored values
+    console.log("\nDone!\n")
+
+    console.log("vnd.vocdoni.entity-name =", await resolverInstance.text(entityId, "vnd.vocdoni.entity-name"));
+    console.log("vnd.vocdoni.languages =", await resolverInstance.text(entityId, "vnd.vocdoni.languages"));
+    console.log("vnd.vocdoni.meta =", await resolverInstance.text(entityId, "vnd.vocdoni.meta"));
+    console.log("vnd.vocdoni.voting-contract =", await resolverInstance.text(entityId, "vnd.vocdoni.voting-contract"));
+    console.log("vnd.vocdoni.gateway-update =", await resolverInstance.text(entityId, "vnd.vocdoni.gateway-update"));
+    console.log("vnd.vocdoni.process-ids.active =", await resolverInstance.text(entityId, "vnd.vocdoni.process-ids.active"));
+    console.log("vnd.vocdoni.process-ids.ended =", await resolverInstance.text(entityId, "vnd.vocdoni.process-ids.ended"));
+    console.log("vnd.vocdoni.news-feed.en =", await resolverInstance.text(entityId, "vnd.vocdoni.news-feed.en"));
+    console.log("vnd.vocdoni.news-feed.fr =", await resolverInstance.text(entityId, "vnd.vocdoni.news-feed.fr"));
+    console.log("vnd.vocdoni.entity-description.en =", await resolverInstance.text(entityId, "vnd.vocdoni.entity-description.en"));
+    console.log("vnd.vocdoni.entity-description.fr =", await resolverInstance.text(entityId, "vnd.vocdoni.entity-description.fr"));
+    console.log("vnd.vocdoni.avatar =", await resolverInstance.text(entityId, "vnd.vocdoni.avatar"));
+}
+
+async function main() {
+    await registerEntity()
+}
+
+main()
+    .then(() => console.log("DONE"))
+    .catch(err => console.error(err))
