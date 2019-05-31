c6d82bc252397b207d791f79b8a5ab419829b974
ledfuse <ledfuse@users.noreply.github.com>
2019-05-31 17:22:54 +0200

    Adding an additional type

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+// Classes
 export { default as VotingProcess } from "./dvote/voting-process"
-export { default as EntityResolver } from "./dvote/entity-resolver"
+export { default as EntityResolver, TextRecordKeys, TextListRecordKeys } from "./dvote/entity-resolver"
 export { default as Census } from "./dvote/census"
 export { default as Gateway } from "./dvote/gateway"
 
-export { EntityMetadata } from "./lib/metadata-types"
+// Types
+export { EntityMetadata, EntityResolverFields } from "./lib/metadata-types"
