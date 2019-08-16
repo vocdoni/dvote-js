9f5aab1fcab71122089872fc93b4875addba742a
Jør∂¡ <itsbrickpop@gmail.com>
2019-08-16 22:20:08 +0200

    Adding exports

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,11 @@ export * from "./api/entity"
 export * from "./api/vote"
 export * from "./api/census"
 export { DVoteGateway, CensusGateway, Web3Gateway } from "./net/gateway"
+export * from "./net/contract"
 
 // Wrappers
 export { default as ContentURI } from "./util/content-uri"
+export { default as ContentHashedURI } from "./util/content-hashed-uri"
 export { default as GatewayURI } from "./util/gateway-uri"
 
 // Util
