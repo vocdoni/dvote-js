eae20e6666337cc599ccf35fde5716476627a936
Jør∂¡ <itsbrickpop@gmail.com>
2019-09-03 17:01:32 +0200

    Removing hardcoded Bootnodes and fetching from the Blockchain instead

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,7 @@
-export const defaultBootnodeContentUri = "https://bootnodes.github.io/gateways.json"
 export const ipfsGatewayListUri = "https://ipfs.github.io/public-gateway-checker/gateways.json"
 
 export const entityResolverEnsDomain = "entity-resolver.vocdoni.eth"
 export const votingProcessEnsDomain = "voting-process.vocdoni.eth"
+
+export const vocdoniHomesteadEntityId = "0x0"
+export const vocdoniGoerliEntityId = "0x180dd5765d9f7ecef810b565a2e5bd14a3ccd536c442b3de74867df552855e85"
