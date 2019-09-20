9beba348437c1e651344adedf0fcf173ba7daa27
Jør∂¡ <itsbrickpop@gmail.com>
2019-09-21 00:26:14 +0200

    Update the testing

--- test/unit/census.ts
+++ test/unit/census.ts
@@ -12,19 +12,19 @@ describe("Census", () => {
     describe("Poseidon hashing", () => {
         it("Should hash public keys properly", () => {
             // 1
-            let wallet = Wallet.fromMnemonic("fly cheap color olive setup rigid april forum over grief predict pipe toddler argue give")
+            let wallet = Wallet.fromMnemonic("fly cheap color olive setup rigid april forum over grief predict pipe toddler argue give", "m/44'/60'/0'/0/0")
             let pubKey = wallet["signingKey"].publicKey  // 0x04...
             const hash1 = digestHexClaim(pubKey)
             expect(hash1).to.equal("ILtGMAKtwhkcRM80o+L3FLeQWTUx9wN+rhIpxzFZsHo=")
 
             // 2
-            wallet = Wallet.fromMnemonic("kangaroo improve enroll almost since stock travel grace improve welcome orbit decorate govern hospital select")
+            wallet = Wallet.fromMnemonic("kangaroo improve enroll almost since stock travel grace improve welcome orbit decorate govern hospital select", "m/44'/60'/0'/0/0")
             pubKey = wallet["signingKey"].publicKey  // 0x04...
             const hash2 = digestHexClaim(pubKey)
             expect(hash2).to.equal("L8Y2jIRV7BjNw7pFrIzJ/PR41Bx3RcTPR7FAN44Gug==")
 
             // 3
-            wallet = Wallet.fromMnemonic("soup sunset inhale lend eagle hold reduce churn alpha torch leopard phrase unfold crucial soccer")
+            wallet = Wallet.fromMnemonic("soup sunset inhale lend eagle hold reduce churn alpha torch leopard phrase unfold crucial soccer", "m/44'/60'/0'/0/0")
             pubKey = wallet["signingKey"].publicKey  // 0x04...
             const hash3 = digestHexClaim(pubKey)
             expect(hash3).to.equal("ITbzM8xwQF+n6yHftMu7mUlVxmIvvs5Sa7K9aOCtKQ==")
@@ -35,7 +35,7 @@ describe("Census", () => {
         })
 
         it("Should fail on invalid hex strings", () => {
-            let wallet = Wallet.fromMnemonic("soul frequent purity regret noble husband weapon scheme cement lamp put regular envelope physical entire")
+            let wallet = Wallet.fromMnemonic("soul frequent purity regret noble husband weapon scheme cement lamp put regular envelope physical entire", "m/44'/60'/0'/0/0")
             let pubKey = wallet["signingKey"].publicKey
 
             expect(() => {
