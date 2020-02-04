c8a13647c02f28ac04729fd2d925ec2c4336b1b1
divdevdoe <divdevdoe@riseup.net>
2020-02-04 17:48:49 +0100

    Adds ProcessResultItem to types

--- src/types.ts
+++ src/types.ts
@@ -4,7 +4,7 @@
 
 export * from "./models/common"
 export { EntityMetadata } from "./models/entity"
-export { ProcessMetadata, ProcessResults, ProcessType, VochainProcessState } from "./models/voting-process"
+export { ProcessMetadata, ProcessResults, ProcessType, ProcessResultItem, VochainProcessState } from "./models/voting-process"
 export {
     DVoteSupportedApi,
     FileApiMethod,
