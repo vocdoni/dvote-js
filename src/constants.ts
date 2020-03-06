0093f5a95da6a6744b4f33de1b1898c60ea813be
divdevdoe <divdevdoe@riseup.net>
2020-03-06 20:04:09 +0100

    Updates block time value

--- src/constants.ts
+++ src/constants.ts
@@ -7,5 +7,5 @@ export const vocdoniHomesteadEntityId = "0x0"
 export const vocdoniGoerliEntityId = "0x180dd5765d9f7ecef810b565a2e5bd14a3ccd536c442b3de74867df552855e85"
 
 export const SIGNATURE_TIMESTAMP_TOLERANCE = 60 // seconds
-export const VOCHAIN_BLOCK_TIME = 5 // seconds
+export const VOCHAIN_BLOCK_TIME = 10 // seconds
 export const CENSUS_MAX_BULK_SIZE = 400 // # of claims per addClaimBulk request
