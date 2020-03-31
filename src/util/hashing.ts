29a2279082d586e5fac58701d071406d21d77379
Jør∂¡ <itsbrickpop@gmail.com>
2020-03-31 22:20:49 +0200

    Hotfix: provide an updated implementation of poseidon hash, since recent dependencies broke the builds

--- src/util/hashing.ts
+++ src/util/hashing.ts
@@ -1,44 +1,52 @@
-import { bigInt, bn128 } from 'snarkjs'
-import { createHash } from "circomlib/src/poseidon"
+import { createHash } from "circomlib/src/poseidon.js"
+import { leBuff2int } from "circomlib/src/utils.js"
+import * as bigInt from "big-integer"
 
-const F = bn128.Fr
+const Q = bigInt("21888242871839275222246405745257275088548364400416034343698204186575808495617")
+const T = 6
 
-export function hash(arr) {
-    const poseidonHash = createHash(6, 8, 57)
+export function hash(arr: bigInt.BigInteger[]) {
+    const poseidonHash = createHash()
     return poseidonHash(arr)
 }
 
-export function multiHash(arr) {
+export function multiHash(arr: bigInt.BigInteger[]): bigInt.BigInteger {
     // TODO: check bigints inside finite field
 
-    let r = bigInt(0)
-    for (let i = 0; i < arr.length; i += 5) {
-        const fiveElems = []
-        for (let j = 0; j < 5; j++) {
-            if (i + j < arr.length) {
-                fiveElems.push(arr[i + j])
-            } else {
-                fiveElems.push(bigInt(0))
-            }
+    let r = bigInt(1)
+    for (let i = 0; i < arr.length; i += T - 1) {
+        const toHash: bigInt.BigInteger[] = []
+        let j = 0
+        for (; j < T - 1; j++) {
+            if (i + j >= arr.length) break
+            toHash[j] = arr[i + j]
         }
-        const ph = hash(fiveElems)
-        r = F.add(r, ph)
+        toHash[j] = r
+        j++
+        for (; j < T; j++) {
+            toHash[j] = bigInt(0)
+        }
+
+        const ph = hash(toHash)
+
+        r = r.add(ph)
+        r = r.mod(Q)
     }
-    return F.affine(r)
+    return r
 }
 
-export function hashBuffer(msgBuff: number[] | Uint8Array): bigInt {
+export function hashBuffer(msgBuff: number[] | Uint8Array) {
     const n = 31
     const msgArray = []
     const fullParts = Math.floor(msgBuff.length / n)
 
     for (let i = 0; i < fullParts; i++) {
-        const v = bigInt.leBuff2int(msgBuff.slice(n * i, n * (i + 1)))
+        const v = leBuff2int(msgBuff.slice(n * i, n * (i + 1)))
         msgArray.push(v)
     }
 
     if (msgBuff.length % n !== 0) {
-        const v = bigInt.leBuff2int(msgBuff.slice(fullParts * n))
+        const v = leBuff2int(msgBuff.slice(fullParts * n))
         msgArray.push(v)
     }
     return multiHash(msgArray)
--- test/unit/census.ts
+++ test/unit/census.ts
@@ -10,66 +10,58 @@ addCompletionHooks()
 describe("Census", () => {
 
     describe("Poseidon hashing", () => {
-        // TODO: update once circomlib JS is also updated
-        it("Should be updated once circomlib JS is also updated")
+        // // TODO: update once circomlib JS is also updated
+        // it("Should be updated once circomlib JS is also updated")
 
-        // it("Should hash public keys properly", () => {
-        //     // 1
-        //     let wallet = Wallet.fromMnemonic("fly cheap color olive setup rigid april forum over grief predict pipe toddler argue give", "m/44'/60'/0'/0/0")
-        //     let pubKey = wallet["signingKey"].publicKey  // 0x04...
-        //     const hash1 = digestHexClaim(pubKey)
-        //     expect(hash1).to.equal("A69fOQ6ObtcUgXMdFgMSbQnhk4+F6tnC/NyYokgZrc0=")
+        it("Should hash public keys properly", () => {
+            let hexClaim1 = "0x045a126cbbd3c66b6d542d40d91085e3f2b5db3bbc8cda0d59615deb08784e4f833e0bb082194790143c3d01cedb4a9663cb8c7bdaaad839cb794dd309213fcf30"
+            let hexHash1 = digestHexClaim(hexClaim1)
+            expect(hexHash1).to.equal("EB2a00pTkDYoqlnPUQ49D8wUZ41YPwEVpaoaLr2YY5w=")
 
-        //     // 2
-        //     wallet = Wallet.fromMnemonic("kangaroo improve enroll almost since stock travel grace improve welcome orbit decorate govern hospital select", "m/44'/60'/0'/0/0")
-        //     pubKey = wallet["signingKey"].publicKey  // 0x04...
-        //     const hash2 = digestHexClaim(pubKey)
-        //     expect(hash2).to.equal("HuboX6y+3LHrhFy1cfQ/u6QAY9PvB6Yg98aELzJH7kU=")
+            let hexClaim2 = "0x049969c7741ade2e9f89f81d12080651038838e8089682158f3d892e57609b64e2137463c816e4d52f6688d490c35a0b8e524ac6d9722eed2616dbcaf676fc2578"
+            let hexHash2 = digestHexClaim(hexClaim2)
+            expect(hexHash2).to.equal("HOONvrHcCA8KgfirpKKk1RuHUG3NZimRc+9NcJbJuI8=")
 
-        //     // 3
-        //     wallet = Wallet.fromMnemonic("soup sunset inhale lend eagle hold reduce churn alpha torch leopard phrase unfold crucial soccer", "m/44'/60'/0'/0/0")
-        //     pubKey = wallet["signingKey"].publicKey  // 0x04...
-        //     const hash3 = digestHexClaim(pubKey)
-        //     expect(hash3).to.equal("LQ5YLnHQiYagbk3f1YfQbNEM3TZNddKyulHpUpTzCw4=")
+            let hexClaim3 = "0x049622878da186a8a31f4dc03454dbbc62365060458db174618218b51d5014fa56c8ea772234341ae326ce278091c39e30c02fa1f04792035d79311fe3283f1380"
+            let hexHash3 = digestHexClaim(hexClaim3)
+            expect(hexHash3).to.equal("KdzkitvXvJSqndKmRXAYBFZamdOrN+lFyEGKeYYGJeg=")
 
-        //     // 4
-        //     wallet = Wallet.fromMnemonic("soul frequent purity regret noble husband weapon scheme cement lamp put regular envelope physical entire", "m/44'/60'/0'/0/0")
-        //     pubKey = wallet["signingKey"].publicKey  // 0x04...
-        //     const hash4 = digestHexClaim(pubKey)
-        //     expect(hash4).to.equal("DJRuAQL3tnEqD4m+hvQLX4cXkeSgmrxVLR2bUc6ofCI=")
+            let hexClaim4 = "0x0420606a7dcf293722f3eddc7dca0e2505c08d5099e3d495091782a107d006a7d64c3034184fb4cd59475e37bf40ca43e5e262be997bb74c45a9a723067505413e"
+            let hexHash4 = digestHexClaim(hexClaim4)
+            expect(hexHash4).to.equal("L3Y/6iJWtc6DyOS+Wad8tFlh8kiZO5BLCOhTHgSpIlc=")
 
-        //     expect(hash1).to.not.equal(hash2)
-        //     expect(hash1).to.not.equal(hash3)
-        //     expect(hash2).to.not.equal(hash3)
-        // })
+            expect(hexHash1).to.not.equal(hexHash2)
+            expect(hexHash1).to.not.equal(hexHash3)
+            expect(hexHash2).to.not.equal(hexHash3)
+        })
 
-        // it("Should fail on invalid hex strings", () => {
-        //     let wallet = Wallet.fromMnemonic("soul frequent purity regret noble husband weapon scheme cement lamp put regular envelope physical entire", "m/44'/60'/0'/0/0")
-        //     let pubKey = wallet["signingKey"].publicKey
+        it("Should fail on invalid hex strings", () => {
+            let wallet = Wallet.fromMnemonic("soul frequent purity regret noble husband weapon scheme cement lamp put regular envelope physical entire", "m/44'/60'/0'/0/0")
+            let pubKey = wallet["signingKey"].publicKey
 
-        //     expect(() => {
-        //         digestHexClaim(pubKey)
-        //     }).to.not.throw
+            expect(() => {
+                digestHexClaim(pubKey)
+            }).to.not.throw
 
-        //     expect(() => {
-        //         digestHexClaim("hello world 1234")
-        //     }).to.throw
+            expect(() => {
+                digestHexClaim("hello world 1234")
+            }).to.throw
 
-        //     expect(() => {
-        //         digestHexClaim("!\"·$%&")
-        //     }).to.throw
+            expect(() => {
+                digestHexClaim("!\"·$%&")
+            }).to.throw
 
-        //     expect(() => {
-        //         digestHexClaim("")
-        //     }).to.throw
+            expect(() => {
+                digestHexClaim("")
+            }).to.throw
 
-        //     expect(() => {
-        //         digestHexClaim("ZXCVBNM")
-        //     }).to.throw
+            expect(() => {
+                digestHexClaim("ZXCVBNM")
+            }).to.throw
 
-        //     expect(() => {
-        //         digestHexClaim(",.-1234")
-        //     }).to.throw
-        // })
+            expect(() => {
+                digestHexClaim(",.-1234")
+            }).to.throw
+        })
     })
 })
