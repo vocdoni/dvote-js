cdf50276a8651834385b3adf812426ebc3b908a0
emmdim <emmdim@users.noreply.github.com>
2021-03-08 20:59:38 +0100

    Adds secretbox symmetric encrpytion, closes #22

--- src/util/encryption.ts
+++ src/util/encryption.ts
@@ -2,9 +2,11 @@ import { Buffer } from "buffer/"
 import * as tweetnacl from 'tweetnacl'
 import * as sealedbox from 'tweetnacl-sealedbox-js'
 import { hexStringToBuffer } from "./encoding"
+import { utils, } from "ethers"
 
 (tweetnacl as any).sealedbox = sealedbox
 
+const newNonce = () => tweetnacl.randomBytes(tweetnacl.secretbox.nonceLength);
 export class Asymmetric {
     /**
      * Encrypts the given buffer with NaCl SealedBox using the given hex public key.
@@ -116,6 +118,131 @@ export class Asymmetric {
 
         const decryptedBytes = sealedbox.open(encryptedBytes, keyPair.publicKey, keyPair.secretKey)
         if (!decryptedBytes) throw new Error("The message can't be decrypted with the given private key")
+        return new Buffer(decryptedBytes).toString()
+    }
+}
+
+export class Symmetric {
+    /**
+     * Encrypts the given buffer with NaCl SecretBox using the given passpahrase and a random nonce.
+     * Returns a Buffer containing `nonce[24] + cipherText[]`.
+     * @param messageBytes The payload to encrypt
+     * @param passphrase The secret key in string format
+     */
+    static encryptRaw(messageBytes: Buffer, passphrase: string): Buffer {
+        if (!(messageBytes instanceof Buffer))
+            throw new Error("Please, use a Buffer instance from require('buffer/') to pass the messageBytes")
+        else if (typeof passphrase != "string")
+            throw new Error("Invalid passphrase")
+
+        const key = Buffer.from(passphrase, "utf-8")
+        const keyDigest = utils.sha256(key)
+        const nonce = newNonce()
+
+        const box = tweetnacl.secretbox(messageBytes, nonce, hexStringToBuffer(keyDigest))
+
+        const fullMessage = new Uint8Array(nonce.length + box.length);
+        fullMessage.set(nonce);
+        fullMessage.set(box, nonce.length);
+
+        return Buffer.from(fullMessage)
+    }
+
+    /**
+     * Encrypts the given buffer with NaCl SecretBox using the given passpahrase and a random nonce.
+     * Returns a base64 string with the encrypted payload.
+     * @param messageBytes The payload to encrypt
+     * @param passphrase The secret key in string format
+     */
+    static encryptBytes(messageBytes: Buffer, passphrase: string): string {
+        if (!(messageBytes instanceof Buffer))
+            throw new Error("Please, use a Buffer instance from require('buffer/') to pass the messageBytes")
+        else if (typeof passphrase != "string")
+            throw new Error("Invalid passphrase")
+
+        const encryptedMessage = this.encryptRaw(messageBytes, passphrase)
+
+        return Buffer.from(encryptedMessage).toString("base64")
+    }
+
+    /**
+     * Encrypts the given string with NaCl SecretBox using the given passpahrase and a random nonce.
+     * Returns a base64 string with the encrypted payload.
+     * @param message The payload to encrypt
+     * @param passphrase The secret key in string format
+     */
+    static encryptString(message: string, passphrase: string): string {
+        if (typeof message != "string" || typeof passphrase != "string")
+            throw new Error("Invalid parameters")
+
+        const messageBytes = Buffer.from(message)
+        const encryptedMessage = this.encryptRaw(messageBytes, passphrase)
+
+        return Buffer.from(encryptedMessage).toString("base64")
+    }
+
+    /**
+     * Decrypts the given buffer with NaCl SecretBox using the given passpahrase.
+     * Returns the original buffer.
+     * @param encryptedBytes The payload to decrypt
+     * @param passphrase The secret key in string format
+     */
+    static decryptRaw(encryptedBytes: Buffer, passphrase: string): Buffer {
+        if (!(encryptedBytes instanceof Buffer))
+            throw new Error("Please, use a Buffer instance from require('buffer/') to pass the encryptedBytes")
+        else if (typeof passphrase != "string")
+            throw new Error("Invalid passaphrase")
+
+        const key = Buffer.from(passphrase, "utf-8")
+        const keyDigest = utils.sha256(key)
+        const keyDigestBytes = hexStringToBuffer(keyDigest)
+
+        const nonce = encryptedBytes.slice(0, tweetnacl.secretbox.nonceLength);
+        const message = encryptedBytes.slice(
+            tweetnacl.secretbox.nonceLength,
+            encryptedBytes.length
+        )
+        
+
+        const decrypted = tweetnacl.secretbox.open(message, nonce, keyDigestBytes);
+
+        if (!decrypted) {
+            throw new Error("Could not decrypt message");
+        }
+
+        return new Buffer(decrypted)
+    }
+
+    /**
+     * Decrypts the given base64 string with NaCl SecretBox using the given passpahrase and a random nonce.
+     * Returns the original buffer.
+     * @param encryptedBase64 The payload to decrypt
+     * @param passphrase The secret key in string format
+     */
+    static decryptBytes(encryptedBase64: string, passphrase: string): Buffer {
+        if (typeof encryptedBase64 != "string" || typeof passphrase != "string")
+            throw new Error("Invalid parameters")
+
+        const encryptedBytes = Buffer.from(encryptedBase64, "base64")
+
+        return this.decryptRaw(encryptedBytes, passphrase)
+    }
+
+    /**
+     * Decrypts the given base64 string with NaCl SecretBox using the given passpahrase and a random nonce.
+     * Returns the original string.
+     * @param encryptedBase64 The payload to decrypt
+     * @param passphrase The secret key in string format
+     */
+    static decryptString(encryptedBase64: string, passphrase: string): string {
+        if (typeof encryptedBase64 != "string" || typeof passphrase != "string")
+            throw new Error("Invalid parameters")
+
+
+        const encryptedBytes = Buffer.from(encryptedBase64, "base64")
+        const decryptedBytes = this.decryptRaw(encryptedBytes,passphrase)
+
+        if (!decryptedBytes) throw new Error("The message can't be decrypted with the given passphrase")
         return new Buffer(decryptedBytes).toString()
     }
 }
