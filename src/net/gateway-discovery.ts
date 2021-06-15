b1c829f63b7ee8d8256170aeea56b3c6be49b116
Marc Velmer <marc1987@gmail.com>
2021-06-15 17:42:56 +0200

    Fixed tricky way of doing all `Promises.all` with correct function `Promises.allSettled`

--- src/net/gateway-discovery.ts
+++ src/net/gateway-discovery.ts
@@ -137,7 +137,7 @@ export class GatewayDiscovery {
         }).map((gw: IWeb3Gateway) => gw.getBlockNumber())
 
         if (web3getBlockNumberCandidates.length) {
-            await Promise.all(web3getBlockNumberCandidates.map(p => p.catch(e => e))).then()
+            await Promise.allSettled(web3getBlockNumberCandidates).then()
         }
 
         healthyNodes.web3.sort((a: IWeb3Gateway, b: IWeb3Gateway) => {
