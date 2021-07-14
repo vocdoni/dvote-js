d3078807873a1b8fe27207471ff6ae41fc409c2b
Jør∂¡ <itsbrickpop@gmail.com>
2021-07-14 14:40:19 +0100

    Updating the example web project

---
+++ example/web/lib/net.ts
@@ -0,0 +1,17 @@
+import { GatewayDiscovery, GatewayPool } from 'dvote-js'
+
+const BOOTNODE_URI = "https://bootnodes.vocdoni.net/gateways.dev.json"
+const ENVIRONMENT = "dev"
+const NETWORK_ID = "rinkeby"
+const discoveryParams = {
+  bootnodesContentUri: BOOTNODE_URI,
+  networkId: NETWORK_ID as any,
+  environment: ENVIRONMENT as any
+}
+
+export function getClient() {
+  return GatewayDiscovery.run(discoveryParams)
+    .then(result => {
+      return new GatewayPool(result, discoveryParams)
+    })
+}
---
+++ example/web/pages/index.tsx
@@ -0,0 +1,52 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import styles from '../styles/Page.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Vocdoni</title>
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          DVote JS examples
+        </h1>
+
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/*</code>
+        </p>
+
+        <div className={styles.grid}>
+          <Link href="/network">
+            <a className={styles.card}>
+              <h2>Network &rarr;</h2>
+              <p>Basic example of connecting to gateways.</p>
+            </a>
+          </Link>
+
+          <Link href="/metadata">
+            <a className={styles.card}>
+              <h2>Metadata &rarr;</h2>
+              <p>Fetch metadata from entities and processes.</p>
+            </a>
+          </Link>
+
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://docs.vocdoni.io"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Vocdoni
+        </a>
+      </footer>
+    </div>
+  )
+}
+
