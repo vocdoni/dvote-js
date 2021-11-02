68978ef75d9262bbafb59aa7c29140d19dad3089
Marc Velmer <marc1987@gmail.com>
2021-11-02 15:22:33 +0100

    fixed code review

--- packages/common/test/unit/encoding.ts
+++ packages/common/test/unit/encoding.ts
@@ -1,6 +1,6 @@
 import "mocha" // using @types/mocha
 import { expect } from "chai"
-import { addCompletionHooks } from "../mocha-hooks"
+import { addCompletionHooks } from "../../../../shared/test/mocha-hooks"
 import { Buffer } from "buffer/"
 
 import { bigIntToBuffer, bufferToBigInt, hexStringToBuffer, uintArrayToHex } from "../../src"
