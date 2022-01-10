45200afdab1cb3edf2ae2eec7c3ba1c2d649d956
Marc Velmer <marc1987@gmail.com>
2022-01-10 16:39:45 +0100

    Fixed code review

--- packages/census/scripts/modify-snarkjs-package.js
+++ packages/census/scripts/modify-snarkjs-package.js
@@ -6,7 +6,7 @@ let packagePath = path.dirname(require.resolve("snarkjs"));
 while(!fs.existsSync(path.join(packagePath, "package.json"))) {
     const packagePathSplit = packagePath.split(path.sep);
     packagePathSplit.pop();
-    if(packagePath.length === 0 || !packagePath.includes("snarkjs")) {
+    if(packagePath.length === 0 || !packagePathSplit.includes("snarkjs")) {
         console.error("Snarkjs dependency not found");
         process.exit(1);
     }
@@ -16,7 +16,7 @@ packagePath = path.join(packagePath, "package.json")
 
 const packageJson = require(packagePath);
 
-if (packageJson.hasOwnProperty('module')) {
+if (packageJson.hasOwnProperty('module') && packageJson.hasOwnProperty('name') && packageJson.name === "snarkjs") {
     delete packageJson.module;
     fs.writeFileSync(packagePath, JSON.stringify(packageJson, null, 2));
 }
