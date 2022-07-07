71e0bfa8a6968be699750edd0a9dcee5549ad094
Marc Velmer <marc1987@gmail.com>
2022-07-07 17:18:29 +0200

    Added `getUserInfo` in `CspIndexer`

--- packages/csp/src/indexer-calls.ts
+++ packages/csp/src/indexer-calls.ts
@@ -14,22 +14,34 @@ export namespace CspIndexer {
     * @param csp A CSP instance
     * @returns Promise with list of the correspondign election IDs
     */
-         export async function getUserProcesses(userId: string, csp: ICsp): Promise<IndexerResponse> {
-            if (!userId) return Promise.reject(new Error("Invalid user Id"))
-            else if (!csp) return Promise.reject(new Error("Invalid CSP object"))
+     export async function getUserProcesses(userId: string, csp: ICsp): Promise<IndexerResponse> {
+        if (!userId) return Promise.reject(new Error("Invalid user Id"))
+        else if (!csp) return Promise.reject(new Error("Invalid CSP object"))
 
-            return csp.sendRequest(
-                `/auth/elections/indexer/${userId}`,
-                {},{})
-                .then((response) => {
-                    console.log("indexer: "+response)
-                    console.log("indexer: "+JSON.stringify(response,null,2))
-                    if (!('elections' in response)) throw new Error('Invalid csp response')
-                    return response.elections as IndexerResponse
-                })
-                .catch((error) => {
-                    const message = (error.message) ? "Error retrieving user's process: " + error.message : "Error retrieving user's process:"
-                    throw new Error(message)
-                })
-        }
+        return CspIndexer.getUserInfo(userId, csp)
+            .then(userInfo => {
+                if (!('elections' in userInfo)) throw new Error('Invalid csp response')
+                return userInfo.elections as IndexerResponse
+            });
+    }
+
+    /**
+    * Asks the CSP a user ID information
+    * @param userId  The user identifier
+    * @param csp A CSP instance
+    * @returns Promise with info of the user
+    */
+     export async function getUserInfo(userId: string, csp: ICsp) {
+        if (!userId) return Promise.reject(new Error("Invalid user Id"))
+        else if (!csp) return Promise.reject(new Error("Invalid CSP object"))
+
+        return csp.sendRequest(
+            `/auth/elections/indexer/${userId}`,
+            {},{})
+            .then((response) => response)
+            .catch((error) => {
+                const message = (error.message) ? "Error retrieving user info: " + error.message : "Error retrieving user info"
+                throw new Error(message)
+            })
+    }
 }
