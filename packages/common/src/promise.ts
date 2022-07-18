59b6e6c4fcda06579fe393b59ffedd5e687ed389
Marc Velmer <marc1987@gmail.com>
2022-07-18 17:47:59 +0200

    - Added `Promise.any` polyfill as `promiseAny` function in `common` package - Changed `Promise.race` for `promiseAny` polyfill function

--- packages/common/src/promise.ts
+++ packages/common/src/promise.ts
@@ -3,7 +3,7 @@
  *
  * @param proms
  */
-export function allSettled(proms: Array<Promise<any>>): Promise<Array<{ value: any, status: string } | { reason: Error, status: string }>> {
+export function allSettled(proms: Promise<any>[]): Promise<({ value: any, status: string } | { reason: Error, status: string })[]> {
     return Promise.all(proms.map(reflect))
 }
 
@@ -11,4 +11,40 @@ export function reflect<T>(prom: Promise<T>) {
     return prom
         .then((value: T) => ({ value, status: "fulfilled" }))
         .catch((reason: T) => ({ reason, status: "rejected" }))
+}
+
+export function promiseAny<T>(values: Iterable<T | PromiseLike<T>>): Promise<T> {
+    return new Promise<T>((resolve: (value: T) => void, reject: (reason?: any) => void): void => {
+        let hasResolved: boolean = false;
+        const promiseLikes: (T | PromiseLike<T>)[] = [];
+        let iterableCount: number = 0;
+        const rejectionReasons: any[] = [];
+
+        function resolveOnce(value: T): void {
+            if (!hasResolved) {
+                hasResolved = true;
+                resolve(value);
+            }
+        }
+
+        function rejectionCheck(reason?: any): void {
+            rejectionReasons.push(reason);
+            if (rejectionReasons.length >= iterableCount) reject(rejectionReasons);
+        }
+
+        for (const value of values) {
+            iterableCount++;
+            promiseLikes.push(value);
+        }
+
+        for (const promiseLike of promiseLikes) {
+            if ((promiseLike as PromiseLike<T>)?.then !== undefined ||
+                (promiseLike as Promise<T>)?.catch !== undefined) {
+                (promiseLike as Promise<T>)
+                    ?.then((result: T): void => resolveOnce(result))
+                    ?.catch((error?: any): void => undefined);
+                (promiseLike as Promise<T>)?.catch((reason?: any): void => rejectionCheck(reason));
+            } else resolveOnce(promiseLike as T);
+        }
+    });
 }
