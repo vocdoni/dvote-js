ab46c114313f494cfefb2e580ee0706a23e0ad36
Òscar Casajuana <elboletaire@gmail.com>
2022-07-20 17:05:14 +0200

    Added 'build-all' script for easy development usage
    Note that the `test-all` script still works the same. Some styling has
    been done to both tho.
    
    Also, changed npm executions to use the "workspaces" params.

--- scripts/test-all.sh
+++ scripts/test-all.sh
@@ -2,40 +2,17 @@
 
 set -e
 
-PRE_BUILD_PACKAGES_ORDERED=('common' 'contract-wrappers' 'encryption' 'signing' 'hashing' 'data-models')
-PACKAGES=$(find ./packages -type d -maxdepth 1 -mindepth 1)
+SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
-###############################################################################
-
-function install_deps {
-  echo "Installing packages"
-  npm install
-}
-
-function build_all {
-  echo "Building subpackages"
-  for d in ${PRE_BUILD_PACKAGES_ORDERED[@]}
-  do
-    echo "Prebuilding $d"
-    cd ./packages/$d
-    npm run build
-    cd -
-  done
-}
+source $SCRIPT_DIR/build-all.sh
 
 function test_all {
-  echo "Testing subpackages"
-  for d in $PACKAGES
-  do
-    echo "Testing $d"
-    cd $d
-    npm run test
-    cd -
-  done
+    print_task ": Testing subpackages"
+    for d in ${PRE_BUILD_PACKAGES_ORDERED[@]}
+    do
+        echo ":: Testing @vocdoni/$d"
+        npm run test -w ./packages/$d
+    done
 }
 
-###############################################################################
-
-install_deps
-build_all
 test_all
